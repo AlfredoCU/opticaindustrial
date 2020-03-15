c0155e5dafcb99303b8c13b0f36acd512765b673
Alfredo_CU7557 <alfredo_11_cu@hotmail.com>
2020-03-14 23:27:09 -0600

    Updates

---
+++ precache-manifest.aca71a87451f6c8fafe12f20cb9fc0d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1392f4724466c1e93c5190bb44fa8b34",
+    "url": "/opticaindustrial/index.html"
+  },
+  {
+    "revision": "342cb24065f0ebbe975e",
+    "url": "/opticaindustrial/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b8ddcec0315295bc453a",
+    "url": "/opticaindustrial/static/js/2.ce8487d0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/opticaindustrial/static/js/2.ce8487d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "342cb24065f0ebbe975e",
+    "url": "/opticaindustrial/static/js/main.e52773ea.chunk.js"
+  },
+  {
+    "revision": "26c97e2ba1188b2551b4",
+    "url": "/opticaindustrial/static/js/runtime-main.8e0ad354.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/opticaindustrial/static/media/logo.5d5d9eef.svg"
+  }
+]);
